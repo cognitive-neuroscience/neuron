3f7dc0de10d0f9edc9e3f53fa7afb15ed1c45531
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-03 12:15:00 -0400

    Move server creation to a separate file

---
+++ src/server/Server.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+// CreateServer creates a HTTP server
+func CreateServer() {
+	port := os.Getenv("NEURON_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Go HTTP Server running on port %v\n", ":"+port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
