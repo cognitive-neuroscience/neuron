4ee0d616e45eb3d890d81fc8eda5b73ac8b01f1e
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-03 12:15:07 -0400

    Add user service

---
+++ src/services/UserService.go
@@ -0,0 +1,16 @@
+package services
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"github.com/cognitive-neuroscience/neuron/src/database"
+	"github.com/cognitive-neuroscience/neuron/src/models"
+)
+
+// SaveUser saves the user
+func SaveUser(user models.User) models.HTTPErrorStatus {
+	hash := md5.New()
+	hash.Write([]byte(user.Password))
+	user.Password = hex.EncodeToString(hash.Sum(nil))
+	return database.SaveUser(user)
+}
