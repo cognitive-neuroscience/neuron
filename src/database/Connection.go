f358e4abcb74124b325b59d15ec73ceb46f4c89a
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-05 23:36:30 -0400

    Update connection parameters

--- src/database/Connection.go
+++ src/database/Connection.go
@@ -1,15 +1,16 @@
 package database
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql" // MySQL Dialect for GORM
 )
 
 // ConnectDB instantiates a mongoDB connection
 func ConnectDB() {
 	var err error
-	DBConn, err = gorm.Open("mysql", "sharplab:sharplab@(192.168.1.116)/sharplab?charset=utf8mb4")
+	DBConn, err = gorm.Open("mysql", "sharplab:sharplab@(192.168.1.116)/sharplab?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
