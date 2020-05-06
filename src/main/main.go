9e1596c7edeb9749c4a3a46febcf480f1a558659
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-05 23:36:47 -0400

    Updated entry point

--- src/main/main.go
+++ src/main/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/cognitive-neuroscience/neuron/src/database"
-	"github.com/cognitive-neuroscience/neuron/src/router"
 	"github.com/cognitive-neuroscience/neuron/src/server"
 )
 
 func main() {
-	router.RegisterRoutes()
 	database.ConnectDB()
 	database.AutoMigrate()
 	server.CreateServer()
