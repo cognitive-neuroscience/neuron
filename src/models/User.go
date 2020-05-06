6975d0550969e0da494c63a36dc2de73d93145d4
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-05 23:42:01 -0400

    gofmt lint fix

--- src/models/User.go
+++ src/models/User.go
@@ -5,6 +5,6 @@ import "github.com/jinzhu/gorm"
 // User represents a model for a user
 type User struct {
 	gorm.Model
-	Email     string `gorm:"unique;not null"`
-	Password  string
+	Email    string `gorm:"unique;not null"`
+	Password string
 }
