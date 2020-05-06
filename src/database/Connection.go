4e3fc79b1a4eb6f436ac24531cb438e576a24703
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-06 18:42:06 -0400

    Update IP for MySQL connection

--- src/database/Connection.go
+++ src/database/Connection.go
@@ -10,7 +10,7 @@ import (
 // ConnectDB instantiates a mongoDB connection
 func ConnectDB() {
 	var err error
-	DBConn, err = gorm.Open("mysql", "sharplab:sharplab@(192.168.1.116)/sharplab?charset=utf8mb4&parseTime=True&loc=Local")
+	DBConn, err = gorm.Open("mysql", "sharplab:sharplab@(192.168.1.117)/sharplab?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
