fd868276ef160506aa050a7f982c49c945f75e45
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-07 22:49:01 -0400

    Use middleware for rate limiting, compression, panic recovery and logging

--- src/server/Server.go
+++ src/server/Server.go
@@ -4,7 +4,11 @@ import (
 	"os"
 
 	"github.com/cognitive-neuroscience/neuron/src/router"
+	"github.com/gofiber/compression"
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/limiter"
+	"github.com/gofiber/logger"
+	"github.com/gofiber/recover"
 )
 
 // CreateServer creates a HTTP server
@@ -14,6 +18,31 @@ func CreateServer() {
 		port = "8181"
 	}
 	app := fiber.New()
+
+	// Use compression
+	app.Use(compression.New())
+
+	// Use recovery from panic
+	recoverConf := recover.Config{
+		Handler: func(c *fiber.Ctx, err error) {
+			c.SendString(err.Error())
+			c.SendStatus(500)
+		},
+	}
+	app.Use(recover.New(recoverConf))
+
+	// Use logger to log HTTP requests
+	app.Use(logger.New())
+
+	// Use API rate limiter
+	limiterConf := limiter.Config{
+		Max:     10,
+		Timeout: 5,
+	}
+	app.Use(limiter.New(limiterConf))
+
+	// Register app routes
 	router.RegisterRoutes(app)
+
 	app.Listen(port)
 }
