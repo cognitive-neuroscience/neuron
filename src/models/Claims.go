181c19c9f5860dea309ef4e3902de03d543fcba6
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-09 23:38:44 -0400

    Update Claims models

--- src/models/Claims.go
+++ src/models/Claims.go
@@ -4,6 +4,7 @@ import jwt "github.com/dgrijalva/jwt-go"
 
 // Claims represents a model for JWT
 type Claims struct {
-	Email string `json:"email"`
+	UserID uint
+	Email  string
 	jwt.StandardClaims
 }
