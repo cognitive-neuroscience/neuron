ec7ca6c762bb316f4649656caf6924d01bc2af3c
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-09 23:40:28 -0400

    Add upload controller

---
+++ src/controllers/UploadController.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/cognitive-neuroscience/neuron/src/middleware"
+	"github.com/cognitive-neuroscience/neuron/src/models"
+	"github.com/cognitive-neuroscience/neuron/src/services"
+	"github.com/gofiber/fiber"
+)
+
+// UploadController represents the entry point for the Upload API
+func UploadController(c *fiber.Ctx) {
+	middleware.AddHeaders(c)
+
+	if c.Method() != "OPTIONS" {
+		status := middleware.ValidateToken(c, strings.ReplaceAll(c.Get("Authorization"), "Bearer ", ""))
+		if status != http.StatusOK {
+			c.SendStatus(status)
+			return
+		}
+		switch c.Method() {
+		case "POST":
+			doUpload(c)
+			break
+		default:
+			c.Status(http.StatusMethodNotAllowed).JSON(&models.HTTPErrorStatus{Status: http.StatusMethodNotAllowed, Message: http.StatusText(http.StatusMethodNotAllowed)})
+			break
+		}
+	} else {
+		c.SendStatus(http.StatusOK)
+	}
+}
+
+func doUpload(c *fiber.Ctx) {
+	experiment := c.Query("experiment")
+
+	switch experiment {
+
+	case "ts":
+		var data []models.TaskSwitchingData
+		if err := c.BodyParser(&data); err != nil {
+			c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+			return
+		}
+		services.SaveTSData(experiment, c.Get("UserID"), data)
+		c.JSON(data)
+		break
+
+	default:
+		c.Status(http.StatusBadRequest).JSON(&models.HTTPErrorStatus{Status: http.StatusBadRequest, Message: http.StatusText(http.StatusBadRequest)})
+		break
+	}
+
+	c.JSON(fiber.Map{"status": "OK"})
+}
