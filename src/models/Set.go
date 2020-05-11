c1894d4d08ee33ae24fe81bcb4754218801fd4c3
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-10 23:09:33 -0400

    Add validators to set model

--- src/models/Set.go
+++ src/models/Set.go
@@ -1,11 +1,22 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
 
 // Set represents a model for an experiment set
 type Set struct {
 	gorm.Model
 	Description string `json:"description" gorm:"not null;default:''"`
-	Code        string `json:"code" gorm:"unique;not null"`
+	Code        string `json:"code" gorm:"unique;not null;default:''"`
 	Tasks       string `json:"tasks" gorm:"not null;default:''"`
+}
+
+// Validate method validates a given model
+func (set Set) Validate(db *gorm.DB) {
+	if set.Code == "" {
+		db.AddError(errors.New("Code cannot be empty"))
+	}
 }
