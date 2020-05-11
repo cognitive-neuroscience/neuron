a552a1080bcc854d79c712c34b32f08830bff042
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-10 23:06:40 -0400

    Add model validators

--- src/models/User.go
+++ src/models/User.go
@@ -1,12 +1,29 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
 
 // User represents a model for a user
 type User struct {
 	gorm.Model
-	Email    string `gorm:"unique;not null"`
-	Password string `gorm:"not null"`
+	Email    string `gorm:"unique_index;not null;default:''"`
+	Password string `gorm:"not null;default:''"`
 	Role     string `gorm:"not null;default:'PARTICIPANT'"`
-	SetCode  string `gorm:"not null;default:'DEFAULT'"`
+	SetCode  string `gorm:"not null;default:''"`
+}
+
+// Validate method validates a given model
+func (user User) Validate(db *gorm.DB) {
+	if user.Email == "" {
+		db.AddError(errors.New("Email Cannot be empty"))
+	}
+	if user.Password == "" {
+		db.AddError(errors.New("Password cannot be empty"))
+	}
+	if user.SetCode == "" {
+		db.AddError(errors.New("SetCode cannot be empty"))
+	}
 }
