5d905de02706d139d4d7caaf150500192ce31617
Nicholas Lee <niconal902@gmail.com>
2020-10-06 21:25:22 -0400

    created initial nback model

--- src/models/TaskData.go
+++ src/models/TaskData.go
@@ -1,18 +1,18 @@
 package models
 
 // TaskSwitchingData represents a  model for the data received from the Task Switching Experiment
-type TaskSwitchingData struct {
-	Trial        int     `gorm:"primary_key;auto_increment"`
-	IsPractice   int     `json:"isPractice"`
-	Color        string  `json:"color"`
-	Digit        int     `json:"digit"`
-	ActualAnswer string  `json:"actualAnswer"`
-	UserAnswer   string  `json:"userAnswer"`
-	ResponseTime float64 `json:"responseTime"`
-	IsCorrect    int     `json:"isCorrect"`
-	Score        int     `json:"score"`
-	ColorMapping string  `json:"colorMapping"`
-}
+// type TaskSwitchingData struct {
+// 	Trial        int     `gorm:"primary_key;auto_increment"`
+// 	IsPractice   int     `json:"isPractice"`
+// 	Color        string  `json:"color"`
+// 	Digit        int     `json:"digit"`
+// 	ActualAnswer string  `json:"actualAnswer"`
+// 	UserAnswer   string  `json:"userAnswer"`
+// 	ResponseTime float64 `json:"responseTime"`
+// 	IsCorrect    int     `json:"isCorrect"`
+// 	Score        int     `json:"score"`
+// 	ColorMapping string  `json:"colorMapping"`
+// }
 
 // StroopTask represents a model for the data received from the Stroop Task
 type StroopTask struct {
@@ -24,4 +24,11 @@ type StroopTask struct {
 	ResponseTime float32 `json:"responseTime"`
 	Score        int     `json:"score"`
 	Set          int     `json:"set"`
+}
+
+// NBack represents a model for the data received from the N-back Task
+// TODO: implement this
+type NBack struct {
+	UserID string
+	Trial  int
 }
