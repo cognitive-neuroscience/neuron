ca5e76af3f9e1e51fa3e5607f944f9676fb57184
Nicholas Lee <niconal902@gmail.com>
2020-10-15 18:32:35 -0400

    added experiment_user download option

--- src/database/DownloadDatabase.go
+++ src/database/DownloadDatabase.go
@@ -33,10 +33,13 @@ func GetTableNames() ([]string, error) {
 // GetTableData receives the experimentCode and taskName, retrieving the data from the assoc table
 // from the database
 func GetTableData(experimentCode string, taskName string) (interface{}, error) {
-	task := strings.ToLower(RemoveWhiteSpace(taskName))
-	tableName := "experiment_" + experimentCode + "_task_" + task
-	return retrieveDataFromTable(tableName, task)
-
+	if experimentCode == "experiment" && taskName == "users" {
+		return retrieveDataFromTable("experiment_users", "experiment_users")
+	} else {
+		task := strings.ToLower(RemoveWhiteSpace(taskName))
+		tableName := "experiment_" + experimentCode + "_task_" + task
+		return retrieveDataFromTable(tableName, task)
+	}
 }
 
 func retrieveDataFromTable(tableName string, taskName string) (interface{}, error) {
@@ -50,6 +53,10 @@ func retrieveDataFromTable(tableName string, taskName string) (interface{}, erro
 	case "nback":
 		slice := []models.NBack{}
 		err = db.Table(tableName).Order("user_id, trial").Find(&slice).Error
+		return slice, err
+	case "experiment_users":
+		slice := []models.ExperimentUser{}
+		err = db.Table(tableName).Find(&slice).Error
 		return slice, err
 	default:
 		return nil, errors.New("Could not get model from task name")
