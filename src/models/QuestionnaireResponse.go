2978d3b1447f5a4081e6fd89a581ef4dbcd40587
Nicholas Lee <nicholaslee@Nicholass-MacBook-Pro.local>
2021-06-22 12:37:06 -0700

    fix: added questions to demographics questionnaire

--- src/models/QuestionnaireResponse.go
+++ src/models/QuestionnaireResponse.go
@@ -2,14 +2,17 @@ package models
 
 // DemographicsQuestionnaireResponse represents the questionnaire responses completed by Mturk participants
 type DemographicsQuestionnaireResponse struct {
-	ExperimentCode     string `json:"experimentCode"`
-	UserID             string `json:"userID"`
-	Age                int    `json:"age"`
-	Sex                string `json:"sex"`
-	SelfIdentification string `json:"selfIdentification"`
-	YearsOfEducation   int    `json:"yearsOfEducation"`
-	HasNeuroConditions bool   `json:"hasNeuroConditions"`
-	HasPsychConditions bool   `json:"hasPsychConditions"`
+	ExperimentCode          string `json:"experimentCode"`
+	UserID                  string `json:"userID"`
+	Age                     int    `json:"age"`
+	Sex                     string `json:"sex"`
+	SelfIdentification      string `json:"selfIdentification"`
+	YearsOfEducation        int    `json:"yearsOfEducation"`
+	HasNeuroConditions      bool   `json:"hasNeuroConditions"`
+	HasPsychConditions      bool   `json:"hasPsychConditions"`
+	CurrentEmploymentStatus string `json:"currentEmploymentStatus"`
+	HouseholdIncome         string `json:"householdIncome"`
+	SocialClass             string `json:"socialClass"`
 }
 
 // FeedbackQuestionnaireResponse represents the feedback given by participants about the experiment
