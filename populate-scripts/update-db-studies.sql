3a13bb686124d8d1e9d925762fb0ba82fbc419d4
Nicholas Lee <niconal902@gmail.com>
2022-01-25 10:40:34 -0800

    feat: update study background for study 40

--- populate-scripts/update-db-studies.sql
+++ populate-scripts/update-db-studies.sql
@@ -93,3 +93,46 @@ UPDATE studies SET
         ]
     }'
 WHERE id IN (9, 10);
+
+UPDATE studies SET
+    config = '{
+        "title": "Welcome to the Sharp Lab",
+        "sections": [
+            {
+                "header": "We study cognition in people with Parkinson''s disease."
+            },
+            {
+                "textContent": "We are inviting you to take part in an <b>online study of cognition</b>. Cognitive symptoms in people with Parkinson''s disease vary immensely from one person to the next, yet <b>we don''t understand why<b>."
+            },
+            {
+                "textContent": "We have developed <b>online brain games</b> to assess different areas of cognitive functioning and thinking (e.g. memory, multi-tasking, etc) in a <b>very large group of people</b> from all over the world. We want to understand why some people develop significant apathy or an inability to multi-task, while others don''t."
+            },
+            {
+                "textContent": "You will be asked to play a series of brain games and fill in some questionnaires. All of this is done online (no tablets or smartphones, only computers)."
+            },
+            {
+                "textContent": "This takes <b>less than two hours</b>, and you can start and stop as you wish by closing the window."
+            },
+            {
+                "textContent": "Please try to complete the whole study <b>within one week</b>. You can see your progress in your dashboard when you first log in."
+            },
+            {
+                "textContent": "To proceed to the Consent Form and decide if you want to participate:"
+            },
+            {
+                "textContent": "1. <b>If you have never done a study with us</b>, please register below to log in.",
+                "indent": 1
+            },
+            {
+                "textContent": "2. If you already <b>have an account</b>, please log in to continue onto the study.",
+                "indent": 1
+            },
+            {
+                "textContent": "This will take you to the consent form. <b>Your email will NOT be saved if you decline to participate</b>."
+            },
+            {
+                "textContent": "For any questions, please email Dr. Sharp: sharplab.neuro@mcgill.ca"
+            }
+        ]
+    }'
+WHERE id IN (40);
