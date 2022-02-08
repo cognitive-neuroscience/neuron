28b0efc0e24e0e0278bc54890a86aa12ab99b5b9
Nick Lee <nicholas.lee2@mail.mcgill.ca>
2022-02-08 11:27:54 -0500

    feat: added basic metadata for face name association task

--- populate-scripts/update-db.sql
+++ populate-scripts/update-db.sql
@@ -16700,3 +16700,80 @@ UPDATE tasks SET
         ]
     }'
 WHERE id = 51;
+-- Face Name Association
+UPDATE tasks SET
+    from_platform = "PSHARPLAB",
+    task_type = "EXPERIMENTAL",
+    name = "Face Name Association Task",
+    description = "Participants learn face name pairs and are then tested on these pairs",
+    external_url = "",
+    config = '{
+        "config": {},
+        "metadata": [
+            {
+                "component": "DISPLAYCOMPONENT",
+                "content": {
+                    "title": "Welcome to the Face Name Game",
+                    "sections": [
+                        {
+                            "sectionType": "text",
+                            "textContent": "This is some placeholder text. It will be replaced later"
+                        }
+                    ],
+                    "buttons": {
+                        "isStart": true,
+                        "previousDisabled": true,
+                        "nextDisabled": false
+                    }
+                }
+            },
+            {
+                "component": "FACENAMEASSOCIATIONCOMPONENT",
+                "config": {
+                    "isPractice": true,
+                    "phase": "learning-phase",
+                    "maxResponseTime": 10000,
+                    "stimulusSet": 1,
+                    "interTrialDelay": 500,
+                    "durationStimulusPresented": 3000,
+                    "stimuliConfig": {
+                        "type": "generated",
+                        "stimuli": null
+                    }
+                }
+            },
+            {
+                "component": "DISPLAYCOMPONENT",
+                "content": {
+                    "title": "Congrats!",
+                    "sections": [
+                        {
+                            "sectionType": "text",
+                            "textContent": "This is more placeholder text."
+                        }
+                    ],
+                    "buttons": {
+                        "isStart": true,
+                        "previousDisabled": true,
+                        "nextDisabled": false
+                    }
+                }
+            },
+            {
+                "component": "FACENAMEASSOCIATIONCOMPONENT",
+                "config": {
+                    "isPractice": true,
+                    "phase": "test-phase",
+                    "maxResponseTime": 10000,
+                    "stimulusSet": 1,
+                    "interTrialDelay": 500,
+                    "durationStimulusPresented": 3000,
+                    "stimuliConfig": {
+                        "type": "generated",
+                        "stimuli": null
+                    }
+                }
+            }
+        ]
+    }'
+WHERE id = 52;
