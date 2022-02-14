24fe9ccfd1e10c48756a58302c26d337e6d3bd5c
Nick Lee <nicholas.lee2@mail.mcgill.ca>
2022-02-14 15:38:55 -0500

    feat: added script for face name association task

--- populate-scripts/insert-db.sql
+++ populate-scripts/insert-db.sql
@@ -15,11 +15,15 @@ VALUES
             {
                 "component": "DISPLAYCOMPONENT",
                 "content": {
-                    "title": "Welcome to the Face Name Game",
+                    "title": "Welcome to the Face Name Association Game",
                     "sections": [
                         {
                             "sectionType": "text",
-                            "textContent": "This is some placeholder text. It will be replaced later"
+                            "textContent": "You will be shown a face and a name on the screen"
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "You will start by memorizing the phase name pairings. You will see these pairs twice in random order."
                         }
                     ],
                     "buttons": {
@@ -47,11 +51,15 @@ VALUES
             {
                 "component": "DISPLAYCOMPONENT",
                 "content": {
-                    "title": "Congrats!",
+                    "title": "Take a break",
                     "sections": [
                         {
                             "sectionType": "text",
-                            "textContent": "This is more placeholder text."
+                            "textContent": "You have been shown the face name pairs."
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "You will be shown the face name pairs again in a different random order"
                         }
                     ],
                     "buttons": {
@@ -65,6 +73,42 @@ VALUES
                 "component": "FACENAMEASSOCIATIONCOMPONENT",
                 "config": {
                     "isPractice": true,
+                    "phase": "learning-phase",
+                    "maxResponseTime": 10000,
+                    "stimulusSet": 1,
+                    "interTrialDelay": 500,
+                    "durationStimulusPresented": 3000,
+                    "stimuliConfig": {
+                        "type": "generated",
+                        "stimuli": null
+                    }
+                }
+            },
+            {
+                "component": "DISPLAYCOMPONENT",
+                "content": {
+                    "title": "Testing phase",
+                    "sections": [
+                        {
+                            "sectionType": "text",
+                            "textContent": "You have been shown the face name pairs."
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "You will now be tested on the different face name pairings"
+                        }
+                    ],
+                    "buttons": {
+                        "isStart": true,
+                        "previousDisabled": true,
+                        "nextDisabled": false
+                    }
+                }
+            },
+            {
+                "component": "FACENAMEASSOCIATIONCOMPONENT",
+                "config": {
+                    "isPractice": false,
                     "phase": "test-phase",
                     "maxResponseTime": 10000,
                     "stimulusSet": 1,
@@ -73,6 +117,27 @@ VALUES
                     "stimuliConfig": {
                         "type": "generated",
                         "stimuli": null
+                    }
+                }
+            },
+            {
+                "component": "DISPLAYCOMPONENT",
+                "content": {
+                    "title": "Congrats!",
+                    "sections": [
+                        {
+                            "sectionType": "text",
+                            "textContent": "You have completed the entire task"
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "Thank you for your participation"
+                        }
+                    ],
+                    "buttons": {
+                        "isStart": false,
+                        "previousDisabled": true,
+                        "nextDisabled": false
                     }
                 }
             }
--- populate-scripts/update-db.sql
+++ populate-scripts/update-db.sql
@@ -16713,11 +16713,15 @@ UPDATE tasks SET
             {
                 "component": "DISPLAYCOMPONENT",
                 "content": {
-                    "title": "Welcome to the Face Name Game",
+                    "title": "Welcome to the Face Name Association Game",
                     "sections": [
                         {
                             "sectionType": "text",
-                            "textContent": "This is some placeholder text. It will be replaced later"
+                            "textContent": "You will be shown a face and a name on the screen"
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "You will start by memorizing the phase name pairings. You will see these pairs twice in random order."
                         }
                     ],
                     "buttons": {
@@ -16745,11 +16749,15 @@ UPDATE tasks SET
             {
                 "component": "DISPLAYCOMPONENT",
                 "content": {
-                    "title": "Congrats!",
+                    "title": "Take a break",
                     "sections": [
                         {
                             "sectionType": "text",
-                            "textContent": "This is more placeholder text."
+                            "textContent": "You have been shown the face name pairs."
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "You will be shown the face name pairs again in a different random order"
                         }
                     ],
                     "buttons": {
@@ -16763,6 +16771,42 @@ UPDATE tasks SET
                 "component": "FACENAMEASSOCIATIONCOMPONENT",
                 "config": {
                     "isPractice": true,
+                    "phase": "learning-phase",
+                    "maxResponseTime": 10000,
+                    "stimulusSet": 1,
+                    "interTrialDelay": 500,
+                    "durationStimulusPresented": 3000,
+                    "stimuliConfig": {
+                        "type": "generated",
+                        "stimuli": null
+                    }
+                }
+            },
+            {
+                "component": "DISPLAYCOMPONENT",
+                "content": {
+                    "title": "Testing phase",
+                    "sections": [
+                        {
+                            "sectionType": "text",
+                            "textContent": "You have been shown the face name pairs."
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "You will now be tested on the different face name pairings"
+                        }
+                    ],
+                    "buttons": {
+                        "isStart": true,
+                        "previousDisabled": true,
+                        "nextDisabled": false
+                    }
+                }
+            },
+            {
+                "component": "FACENAMEASSOCIATIONCOMPONENT",
+                "config": {
+                    "isPractice": false,
                     "phase": "test-phase",
                     "maxResponseTime": 10000,
                     "stimulusSet": 1,
@@ -16771,6 +16815,27 @@ UPDATE tasks SET
                     "stimuliConfig": {
                         "type": "generated",
                         "stimuli": null
+                    }
+                }
+            },
+            {
+                "component": "DISPLAYCOMPONENT",
+                "content": {
+                    "title": "Congrats!",
+                    "sections": [
+                        {
+                            "sectionType": "text",
+                            "textContent": "You have completed the entire task"
+                        },
+                        {
+                            "sectionType": "text",
+                            "textContent": "Thank you for your participation"
+                        }
+                    ],
+                    "buttons": {
+                        "isStart": false,
+                        "previousDisabled": true,
+                        "nextDisabled": false
                     }
                 }
             }
