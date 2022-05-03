12cc1355fdc6adabc98e5258a0569791958d0dc3
Nicholas Lee <nicholas.lee2@mail.mcgill.ca>
2022-05-03 11:47:49 -0500

    Feat/plt (#25)
    * feat: added preliminary data for plt
    
    * feat: uploaded PLT script

--- populate-scripts/initial-populate.sql
+++ populate-scripts/initial-populate.sql
@@ -554,4 +554,14 @@ VALUES
     "",
     "",
     '{}'
+),
+-- PLT
+(
+    NULL,
+    "PSHARPLAB",
+    "EXPERIMENTAL",
+    "Probabilistic Learning Task",
+    "This task is a probablistic learning task based on the reinforcement learning model. The current version is based on the paper by Frank, Woroch and Curran. (2005). Neuron, 47(4), 495-501.",
+    "",
+    '{}'
 );
