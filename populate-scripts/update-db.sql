e382beca558629733d62fea4a33bb15dbcebbd1f
Nicholas Lee <niconal902@gmail.com>
2022-07-11 17:31:30 +1000

    hotfix: fixed PLT script

--- populate-scripts/update-db.sql
+++ populate-scripts/update-db.sql
@@ -21007,5 +21007,15 @@ UPDATE tasks SET
     name = "PLT 2",
     description = "Probabilistic Learning Task 2",
     external_url = "https://run.pavlovia.org/Sharp_lab/probabilistic-learning-task-2/html/",
-    config = '{}'
+    config = '{
+        "taskConfig": {},
+        "metadata": [
+            {
+                "componentName": "EMBEDDEDPAGECOMPONENT",
+                "componentConfig": {
+                    "externalUrl":  "https://run.pavlovia.org/Sharp_lab/probabilistic-learning-task-2/html/"
+                }
+            }
+        ]
+    }'
 WHERE id = 62;
