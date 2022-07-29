059362884982b1684cc477ac6269f57ca8956f44
Nicholas Lee <niconal902@gmail.com>
2022-07-29 13:05:59 -0700

    feat: uploaded scripts for everyday choice

--- populate-scripts/update-db-studies.sql
+++ populate-scripts/update-db-studies.sql
@@ -220,7 +220,7 @@ WHERE id = 55;
 
 UPDATE studies SET
     config = '{
-        "title": "Welcome to the Sharp Lab | Bienvenu au Labo Sharp (Défiler vers le bas pour la version française)",
+        "title": "Welcome to the Sharp Lab | Bienvenue au Labo Sharp (Défiler vers le bas pour la version française)",
         "sections": [
             {
                 "header": "We study cognition in people with Parkinson''s disease."
@@ -229,19 +229,16 @@ UPDATE studies SET
                 "textContent": "We are inviting you to take part in an <b>online study of cognition</b>. Cognitive symptoms in people with Parkinson''s disease vary immensely from one person to the next, yet <b>we don''t understand why<b>."
             },
             {
-                "textContent": "We have developed <b>online brain games</b> to assess different areas of cognitive functioning and thinking (e.g. memory, multi-tasking, etc) in a <b>very large group of people</b> from all over the world. We want to understand why some people develop significant apathy or an inability to multi-task, while others don''t."
+                "textContent": "We have developed <b>online brain games</b> to assess different areas of cognitive functioning and thinking (e.g. memory, multi-tasking, etc) in a <b>very large group of people</b> from all over the world. We want to understand why some people develop significant apathy or an inability to multi-task, or memory difficulties, while others don''t."
             },
             {
-                "textContent": "Everyone between the ages of 50 to 90 with or without Parkinson''s disease is elligible to participate in this study."
+                "textContent": "Everyone between the ages of 50 to 90 with or without Parkinson''s disease is eligible to participate in this study."
             },
             {
-                "textContent": "You will be asked to play a game of choice between everyday activities and fill in some questionnaires."
+                "textContent": "You will be asked to play a game of choice about everyday activities and fill in some questionnaires. All of this is done online (NO tablets or smartphones, only computers. Sorry!)."
             },
             {
-                "textContent": "You must complete this study on a computer (no tablets or smartphones)."
-            },
-            {
-                "textContent": "The entire study takes <b>less than one hour and a half</b>. Please try to complete it <b>in one sitting</b>, but instructions on how to take breaks will follow."
+                "textContent": "TThe entire study takes <b>less than one hour and a half</b>. Please try to complete it <b>in one sitting</b>, but instructions on how to take breaks will follow."
             },
             {
                 "textContent": "To proceed to the Consent Form and decide if you want to participate:"
@@ -251,7 +248,7 @@ UPDATE studies SET
                 "indent": 1
             },
             {
-                "textContent": "2. If you already <b>have an account</b>, please log in to continue onto the study.",
+                "textContent": "2. If you already <b>have an account</b>, please log in to continue to the consent form.",
                 "indent": 1
             },
             {
@@ -261,48 +258,46 @@ UPDATE studies SET
                 "textContent": "For any questions, please email Dr. Sharp: sharplab.neuro@mcgill.ca"
             },
             {
-                "hr": true
+                "header": "Nous étudions les symptômes cognitifs chez les personnes atteintes de la maladie de Parkinson."
             },
             {
-                "header": "Nous étudions la cognition chez les personnes atteintes de la maladie de Parkinson."
+                "textContent": "Nous vous invitons à prendre part à cette <b>étude de cognition sur le web</b>. Les symptômes cognitifs associés à la maladie de Parkinson varient énormément d''une personne à l''autre, mais nous ne comprenons toujours pas <b>pourquoi<b>."
             },
             {
-                "textContent": "Nous vous invitons à participer à <b>une étude en ligne sur la cognition</b>. Les symptômes cognitifs des personnes atteintes de la maladie de Parkinson varient énormément d''une personne à l''autre, mais <b>nous ne comprenons pas pourquoi.</b>"
+                "textContent": "Nous avons développé des <b>jeux de cerveau sur le web</b> pour évaluer différentes fonctions cognitives (ex. mémoire, multi-tâches, concentration, etc.) dans un <b>très grand échantillon de personnes</b> provenant de partout dans le monde. Nous visons à comprendre pourquoi certaines personnes développent des symptômes cognitifs comme des problèmes de concentration, ou une diminution de la motivation, ou des pertes de mémoire, tandis que d''autres y échappent."
             },
             {
-                "textContent": "Nous avons développé des <b>jeux cérébraux en ligne</b> pour évaluer différents domaines du fonctionnement cognitif et de la pensée (par exemple, la mémoire, la polyvalence, etc.) dans <b>un très grand groupe de personnes</b> du monde entier. Nous voulons comprendre pourquoi certaines personnes développent une apathie importante ou une incapacité à effectuer plusieurs tâches, alors que d''autres ne le font pas."
+                "textContent": "Toutes les personnes âgées de 50 à 90 ans, atteinte ou non atteinte de la maladie de Parkinson, est éligible pour participer à cette étude."
             },
             {
-                "textContent": "Toute le monde âgée de 50 à 90 ans, atteinte ou non de la maladie de Parkinson, peut participer à cette étude."
+                "textContent": "Nous vous demanderons de jouer à un jeu de décisions portant sur des activités quotidiennes et de remplir des questionnaires. Tout ceci est fait en ligne, à partir de votre ordinateur. Malheureusement il n''est pas possible de participer à partir d''un téléphone intelligent ou d''une tablette, désolé!"
             },
             {
-                "textContent": "On vous demandera de jouer à un jeu de choix entre des activités quotidiennes et de remplir quelques questionnaires. Tout cela se fait en ligne (pas de tablettes ni de smartphones, uniquement des ordinateurs)."
+                "textContent": "Cette étude prend <b>moins d''une heure et demie</b> à compléter. SVP essayez de la compléter <b>en une seule fois</b>, mais nous vous fournirons aussi des instructions sur comment prendre des pauses."
             },
             {
-                "textContent": "Cela prend <b>moins d''une heure et demie</b>, et vous pouvez commencer et arrêter comme vous le souhaitez en fermant la fenêtre d''ordinateur."
+                "textContent": "Pour procéder à lire le Formulaire de Consentement et décider si vous voulez participer:"
             },
             {
-                "textContent": "Veuillez essayer de compléter l''étude entière <b>en une seule séance</b>."
-            },
-            {
-                "textContent": "Pour passer au formulaire de consentement et décider si vous voulez participer:"
-            },
-            {
-                "textContent": "1. <b>Si vous n''avez jamais fait d''étude avec nous</b>, veuillez vous inscrire ci-dessous pour vous connecter.",
+                "textContent": "1. <b>Si vous n''avez jamais auparavant participé à une étude sur notre plateforme</b>, SVP vous enregistrer ci-dessous pour vous connecter.",
                 "indent": 1
             },
             {
-                "textContent": "2. Si vous avez déjà <b>un compte</b>, veuillez vous connecter pour continuer sur l''étude.",
+                "textContent": "2. Si vous avez déjà <b>un compte avec nous</b>, SVP vous connecter pour continuer.",
                 "indent": 1
             },
             {
-                "textContent": "Vous accéderez ainsi au formulaire de consentement. <b>Votre adresse électronique ne sera PAS enregistrée si vous refusez de participer</b>."
+                "textContent": "Ceci vous amènera au formulaire de consentement. <b>Votre courriel ne sera PAS sauvegardé si vous décidez de ne pas participer</b>."
             },
             {
-                "textContent": "Pour toute question, veuillez envoyer un courriel à Dr. Sharp: sharplab.neuro@mcgill.ca"
+                "textContent": "Pour quelconque question, n''hésitez pas à envoyer un courriel à Dr. Sharp: sharplab.neuro@mcgill.ca"
             }
         ]
-    }'
+    }
+
+
+
+'
 WHERE id IN (59);
 
 UPDATE studies SET
