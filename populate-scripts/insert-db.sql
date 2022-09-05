6c25ea52ad49a05ba7e205dd4a01e1445d122d22
Nicholas Lee <nicholas.lee2@mail.mcgill.ca>
2022-09-05 16:26:51 -0400

    feat: added new numbers game (#47)
    * feat: added new numbers game
    
    * chore: updated spacing

--- populate-scripts/insert-db.sql
+++ populate-scripts/insert-db.sql
@@ -4,46 +4,10 @@ INSERT INTO tasks (id, from_platform, task_type, name, description, external_url
 VALUES
 (
     NULL,
-    "PAVLOVIA",
-    "EXPERIMENTAL",
-    "DSST 1 FR",
-    "Digit Symbol Substitution Task 1 (FR)",
-    "",
-    '{}'
-),
-(
-    NULL,
-    "PAVLOVIA",
-    "EXPERIMENTAL",
-    "DSST 2 FR",
-    "Digit Symbol Substitution Task 2 (FR)",
-    "",
-    '{}'
-),
-(
-    NULL,
-    "PAVLOVIA",
-    "EXPERIMENTAL",
-    "Corsi FR",
-    "",
-    "",
-    '{}'
-),
-(
-    NULL,
-    "PAVLOVIA",
-    "EXPERIMENTAL",
-    "PLT 1 FR",
-    "",
-    "",
-    '{}'
-),
-(
-    NULL,
-    "PAVLOVIA",
-    "EXPERIMENTAL",
-    "PLT 2 FR",
-    "",
+    "PSHARPLAB",
+    "NAB",
+    "Numbers Game (VERSION 2)",
+    "The participant reads a sequence of numbers and enters them in the same or reverse order",
     "",
     '{}'
 );
