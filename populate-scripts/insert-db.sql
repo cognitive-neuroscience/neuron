769c336228287f37d12bb43f47d114a5187710bf
blankapple <59268551+blankapple@users.noreply.github.com>
2022-11-22 14:08:47 -0500

    New questionnaire to identify source of participant recruitment (#72)
    * New questionnaire to identify source of participant recruitment
    
    * fix: add insert script and minor fixes
    
    Co-authored-by: Nicholas Lee <niconal902@gmail.com>

--- populate-scripts/insert-db.sql
+++ populate-scripts/insert-db.sql
@@ -6,68 +6,8 @@ VALUES
     NULL,
     "PSHARPLAB",
     "INFO_DISPLAY",
-    "Everyday choice middle display",
-    "Informing about two brain games that are left",
+    "HOW DID YOU HEAR ABOUT US",
+    "Questionnaire asking the participant how they were recruited to our study",
     "",
-    '{
-        "taskConfig": {},
-        "metadata": [
-            {
-                "componentName": "INFODISPLAYCOMPONENT",
-                "componentConfig": {
-                    "title": {
-                        "en": "Almost Done!",
-                        "fr": "Presque fini!"
-                    },
-                    "sections": [
-                        {
-                            "header": "",
-                            "textContent": {
-                                "en": "Thank you for your participation so far!",
-                                "fr": "Merci d''avoir participé jusqu''ici!"
-                            }
-                        },
-                        {
-                            "header": "",
-                            "textContent": {
-                                "en": "The only part left is two short Brain Games to complete!",
-                                "fr": "Il vous reste seulement deux petits jeux à compléter!"
-                            }
-                        },
-                        {
-                            "header": "",
-                            "textContent": {
-                                "en": "We understand the study may feel long, but we ask you for just <b>10 more minutes of your time</b>.",
-                                "fr": "Nous comprenons que cela était une longue étude mais nous vous demanderons encore <b>10 minutes</b> pour compléter l''étude au complet."
-                            }
-                        },
-                        {
-                            "header": "",
-                            "textContent": {
-                                "en": "If you would like, you have the option of coming back to the study to complete these two tasks at a later time.",
-                                "fr": "Si vous préférez, vous avez l''option d''arrêter ici et retourner après pour compléter ces deux dernières tâches."
-                            }
-                        },
-                        {
-                            "header": "",
-                            "textContent": {
-                                "en": "Simply re-click on the link that we have sent you on your recruitment email!",
-                                "fr": "Quand vous êtes prêt.e à commencer, allez ouvrir le même lien qu''on vous a envoyé par courriel!"
-                            }
-                        },
-                        {
-                            "header": "",
-                            "textContent": {
-                                "en": "Thank you again for helping us further understand the cognitive symptoms of Parkinson''s disease!",
-                                "fr": "Merci beaucoup encore pour votre contribution à la recherche pour mieux comprendre les symptômes cognitives de la maladie de Parkinson!"
-                            }
-                        }
-                    ],
-                    "buttons": {
-                        "displayContinueButton": true
-                    }
-                }
-            }
-        ]
-    }'
+    '{}'
 );
--- populate-scripts/update-db.sql
+++ populate-scripts/update-db.sql
@@ -26264,3 +26264,75 @@ UPDATE tasks SET
         ]
     }'
 WHERE id = 73;
+-- Form to track sources of participants
+UPDATE tasks SET
+    from_platform = "PSHARPLAB",
+    task_type = "QUESTIONNAIRE",
+    name = "HOW DID YOU HEAR ABOUT US",
+    description = "Questionnaire asking the participant how they were recruited to our study",
+    external_url = "",
+    config = '{
+        "taskConfig": {},
+        "metadata": [
+            {
+                "componentName": "QUESTIONNAIRECOMPONENT",
+                "componentConfig": {
+                    "title": {
+                        "en": "Please choose the best answer which describes how you heard about this study",
+                        "fr": "Veuillez indiquer la réponse qui décrit le mieux comment vous avez découvert cette étude"
+                    },
+                    "questions": [
+                        {
+                            "questionType": "multipleChoiceSelect",
+                            "title": {
+                                "en": "How did you hear about this study",
+                                "fr": "Comment avez-vous découvert cette étude"
+                            },
+                            "key": "recruitment source",
+                            "validation": {
+                                "required": true
+                            },
+                            "multipleChoiceOptions": [
+                                {
+                                    "label": {
+                                        "en": "Email from Quebec Parkinson Network (QPN)",
+                                        "fr": "Courriel du Réseau Parkinson Québec (RPQ)"
+                                    },
+                                    "value": "QPN"
+                                },
+                                {
+                                    "label": {
+                                        "en": "Email from The Canadian Open Parkinson Network (C-OPN)",
+                                        "fr": "Courriel du Réseau Parkinson Canadien Ouvert (RPCO)"
+                                    },
+                                    "value": "C-OPN"
+                                },
+                                {
+                                    "label": {
+                                        "en": "Advertisement from Parkinson Quebec",
+                                        "fr": "Annonce du Parkinson Québec"
+                                    },
+                                    "value": "Parkinson Quebec"
+                                },
+                                {
+                                    "label": {
+                                        "en": "Advertisement from Parkinson Canada",
+                                        "fr": "Annonce du Parkinson Canada"
+                                    },
+                                    "value": "Parkinson Canada"
+                                },
+                                {
+                                    "label": {
+                                        "en": "Not sure/None of the above",
+                                        "fr": "Pas sûr/Aucune de ces réponses"
+                                    },
+                                    "value": "None"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        ]
+    }'
+WHERE id = 74;
