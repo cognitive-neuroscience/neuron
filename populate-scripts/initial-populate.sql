550c3990f0b841be1ee77cc5a14dca862a1b8c27
Nicholas Lee <niconal902@gmail.com>
2023-05-29 16:46:24 -0400

    feat: added information task metadata and supporting questionnaires

--- populate-scripts/initial-populate.sql
+++ populate-scripts/initial-populate.sql
@@ -690,4 +690,52 @@ VALUES
     "Information Task",
     "The participant chooses between exploration and exploitation. They select a card from the deck and either (1) continue selecting a new card with a new value, or (2) select a selected card with known value","",
     '{}'
+),
+-- Information Task Questionnaire Round 2
+(
+    "PSHARPLAB",
+    "QUESTIONNAIRE",
+    "Information Task Questionnaire For Round 2",
+    "",
+    '{}'
+),
+-- Information Task Questionnaire Round 5
+(
+    "PSHARPLAB",
+    "QUESTIONNAIRE",
+    "Information Task Questionnaire For Round 5",
+    "",
+    '{}'
+),
+-- Information Task Questionnaire Round 9
+(
+    "PSHARPLAB",
+    "QUESTIONNAIRE",
+    "Information Task Questionnaire For Round 9",
+    "",
+    '{}'
+),
+-- Information Task Questionnaire Round 13
+(
+    "PSHARPLAB",
+    "QUESTIONNAIRE",
+    "Information Task Questionnaire For Round 13",
+    "",
+    '{}'
+),
+-- Information Task Questionnaire Round 17
+(
+    "PSHARPLAB",
+    "QUESTIONNAIRE",
+    "Information Task Questionnaire For Round 17",
+    "",
+    '{}'
+),
+-- Information Task Questionnaire Round 20
+(
+    "PSHARPLAB",
+    "QUESTIONNAIRE",
+    "Information Task Questionnaire For Round 20",
+    "",
+    '{}'
 );
